3fee3ed714e6ef9dae427e3e5e25756d02818c1a
Faris Mohammed <farismosman@gmail.com>
2015-07-06 16:29:23 +0200

    Kulani| Hans | Faris [chefmodo] adds search button to action bar

--- app/src/test/java/com/chefmodo/HomeActivityTest.java
+++ app/src/test/java/com/chefmodo/HomeActivityTest.java
@@ -1,7 +1,9 @@
 package com.chefmodo;
 
+import android.view.Menu;
 import android.widget.TextView;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.Robolectric;
@@ -11,16 +13,31 @@ import org.robolectric.annotation.Config;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.robolectric.Shadows.shadowOf;
 
 @RunWith(RobolectricGradleTestRunner.class)
 @Config(constants = BuildConfig.class, emulateSdk = 21)
 public class HomeActivityTest {
+
+    private HomeActivity homeActivity;
+
+    @Before
+    public void setUp() throws Exception {
+        homeActivity = Robolectric.buildActivity(HomeActivity.class).create().visible().get();
+    }
+
     @Test
     public void shouldHaveAllUIComponents(){
-        HomeActivity homeActivity = Robolectric.buildActivity(HomeActivity.class).create().get();
         TextView screenTitle = (TextView) homeActivity.findViewById(R.id.screen_title);
 
         assertThat(homeActivity.findViewById(R.id.search_bar), is(notNullValue()));
         assertThat(screenTitle.getText().toString(), is("Chefmodo"));
+    }
+
+    @Test
+    public void shouldHaveSearchInTheActionBar(){
+        Menu homeMenu = shadowOf(homeActivity).getOptionsMenu();
+
+        assertThat(homeMenu.getItem(0).getTitle().toString(), is("SEARCH"));
     }
 }
