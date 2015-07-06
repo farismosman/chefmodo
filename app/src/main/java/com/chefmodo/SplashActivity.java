a83fbc667d14bd2e3e36a9957f9553cfd040200d
Faris Mohammed <farismosman@gmail.com>
2015-07-06 15:04:14 +0200

    Kulani| Hans | Faris [chefmodo] starts the home activity after two seconds

--- app/src/main/java/com/chefmodo/SplashActivity.java
+++ app/src/main/java/com/chefmodo/SplashActivity.java
@@ -1,7 +1,9 @@
 package com.chefmodo;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class SplashActivity extends Activity {
 
@@ -9,5 +11,18 @@ public class SplashActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.splash);
+
+        startHomeActivity();
+    }
+
+    private void startHomeActivity() {
+        final Intent homeIntent = new Intent(this, HomeActivity.class);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(homeIntent);
+            }
+        }, 2000);
     }
 }
--- app/src/test/java/com/chefmodo/SplashActivityTest.java
+++ app/src/test/java/com/chefmodo/SplashActivityTest.java
@@ -1,6 +1,7 @@
 package com.chefmodo;
 
-import org.junit.Before;
+import android.content.Intent;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.Robolectric;
@@ -9,20 +10,19 @@ import org.robolectric.annotation.Config;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.robolectric.Shadows.shadowOf;
 
 @RunWith(RobolectricGradleTestRunner.class)
 @Config(constants = BuildConfig.class, emulateSdk = 21)
 public class SplashActivityTest {
 
-    private SplashActivity mActivity;
-
-    @Before
-    public void setup() {
-        mActivity = Robolectric.buildActivity(SplashActivity.class).create().get();
-    }
-
     @Test
-    public void myActivityAppearsAsExpectedInitially() {
-        assertThat(true, is(true));
+    public void shouldStartTheHomeActivityAfterTwoSeconds() {
+        SplashActivity splashActivity = Robolectric.buildActivity(SplashActivity.class).create().get();
+
+        Intent intent = new Intent(splashActivity.getApplicationContext(), HomeActivity.class);
+        Intent nextStartedActivity = shadowOf(splashActivity).getNextStartedActivity();
+
+        assertThat(nextStartedActivity, is(intent));
     }
 }
